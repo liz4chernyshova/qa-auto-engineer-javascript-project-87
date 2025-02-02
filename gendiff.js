8d811e1bd8b69177211e3264cf71c1c5527580fe
Liza Chernyshova <liz4chernyshova@yandex.ru>
2025-02-02 09:56:00 +0300

    step 2

--- gendiff.js
+++ gendiff.js
@@ -2,8 +2,14 @@
 
 import { program } from 'commander';
 
+
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0');
+
+program.option('-f, --format <type>', 'output format');
+
+program.argument('<filepath1>', 'first configuration file')
+       .argument('<filepath2>', 'second configuration file');
 
 program.parse(process.argv);
